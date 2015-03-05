a0ab77b6045041fb605487d1620b1da30da12499
vassilevamartina <vassilevamartina@gmail.com>
2015-03-04 22:43:24 -0800

    Added game-page

---
+++ spec/controllers/game_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+describe GamesController, :type => :controller do 
+  it "should get page for index" do
+    get :index
+    expect(response).to be_success
+    expect(response).to have_http_status(200)
+  end
+end
